0abc5a5f22b88e6348a9fb7c6fc99a897ecd4c4b
Arthur <arthurfelixpereira647@gmail.com>
2024-03-08 17:11:36 -0300

    Subir aula 01

---
+++ type-booleano.js
@@ -0,0 +1,9 @@
+let cidade = "Campo Largo";
+
+cidade = cidade.toLowerCase();
+
+let cidadeNatal = "Campo Largo";
+
+cidadeNatal = cidadeNatal,toLowerCase();
+
+console.log(cidade===cidadeNatal);
---
+++ type-sring.js
@@ -0,0 +1,13 @@
+const nomeAluna = 'Rebeca';
+const maeAluna = 'julia';
+
+console.log(nomeAluna);
+
+const idadeAluna = 21;
+console.log(nomeAluna*idadeAluna);
+
+//podemos concratenar strings e isso é de certa forma uma operação entre strings
+
+console.log(nomeAluna + maeAluna);
+
+// função anônima
